f6ad88e633f5befb0dcd862ad9c5f9b0f6133d3c
Nathan Langer <nlanger@centurioneurope.co.uk>
2018-09-05 16:49:10 +0100

    VisualGroup Homepage / Central Hub

--- resources/views/Websites/VisualGroup/Pages/welcome.blade.php
+++ resources/views/Websites/VisualGroup/Pages/welcome.blade.php
@@ -10,6 +10,7 @@
                         <div class="col-lg-8">
                             <h1 class="VisualSafety">Signs &amp; Labels</h1>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus eum commodi cupiditate in modi corrupti sunt. Autem ipsa dolorem, ex magni provident quos tenetur sit ipsam, odio impedit optio. Consequuntur!</p>
+                            <button class="VisualSafety medium mt35">Browse</button>
                         </div>
                         <div class="col-lg-4">
                             <img src="/images/category/safety/signs-and-labels.jpg" alt="">
@@ -25,6 +26,7 @@
                         <div class="col-lg-8">
                             <h1 class="VisualSafety">Lockout</h1>
                             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium magnam reprehenderit sint provident natus</p>
+                            <button class="VisualSafety medium mt35">Browse</button>
                         </div>
                         <div class="col-lg-4">
                             <img src="/images/category/safety/lockout-tagout.jpg" alt="">
@@ -42,6 +44,7 @@
                         <div class="col-lg-8">
                             <h1 class="VisualSafety">Tagging</h1>
                             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium magnam reprehenderit sint provident natus</p>
+                            <button class="VisualSafety medium mt35">Browse</button>
                         </div>
                         <div class="col-lg-4">
                             <img src="/images/category/safety/tagging-systems.jpg" alt="">
@@ -57,6 +60,7 @@
                         <div class="col-lg-8">
                             <h1 class="VisualSafety">Shadow Boards</h1>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus eum commodi cupiditate in modi corrupti sunt. Autem ipsa dolorem, ex magni provident quos tenetur sit ipsam, odio impedit optio. Consequuntur!</p>
+                            <button class="VisualSafety medium mt35">Browse</button>
                         </div>
                         <div class="col-lg-4">
                             <img src="/images/category/safety/shadow-boards.jpg" alt="">
@@ -68,6 +72,92 @@
     </div>
     <hr class="mt35">     
 </div>
+</div>
+<div class="grid mt35">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="itemcard-header VisualElectrical"></div>
+                <div class="itemcard-category VisualElectrical">
+                    <h4>Electrical, Oil &amp; Gas</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, repellendus tempora? Culpa nobis, vel debitis praesentium error inventore eaque laborum ut dicta quisquam eos provident maxime quas aliquid nihil dolor!</p>
+                    <button class="medium VisualElectrical mt35">Browse</button>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="itemcard-header VisualSafety"></div>
+                <div class="itemcard-category VisualSafety">
+                    <h4>Construction</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, repellendus tempora? Culpa nobis, vel debitis praesentium error inventore eaque laborum ut dicta quisquam eos provident maxime quas aliquid nihil dolor!</p>
+                    <button class="medium VisualSafety mt35">Browse</button>    
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="itemcard-header VisualQuarrying"></div>
+                <div class="itemcard-category VisualQuarrying">
+                    <h4>Quarrying</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, repellendus tempora? Culpa nobis, vel debitis praesentium error inventore eaque laborum ut dicta quisquam eos provident maxime quas aliquid nihil dolor!</p>
+                    <button class="medium VisualQuarry mt35">Browse</button>         
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="grid mt35">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="itemcard-header VisualFarming"></div>
+                <div class="itemcard-category VisualFarming">
+                    <h4>Farming</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, repellendus tempora? Culpa nobis, vel debitis praesentium error inventore eaque laborum ut dicta quisquam eos provident maxime quas aliquid nihil dolor!</p>
+                    <button class="medium VisualFarm mt35">Browse</button>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="itemcard-header VisualFood"></div>
+                <div class="itemcard-category VisualFood">
+                    <h4>Food &amp; Hygiene</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, repellendus tempora? Culpa nobis, vel debitis praesentium error inventore eaque laborum ut dicta quisquam eos provident maxime quas aliquid nihil dolor!</p>
+                    <button class="medium VisualFood mt35">Browse</button>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="itemcard-header VisualAuto"></div>
+                <div class="itemcard-category VisualAuto">
+                    <h4>Automotive</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, repellendus tempora? Culpa nobis, vel debitis praesentium error inventore eaque laborum ut dicta quisquam eos provident maxime quas aliquid nihil dolor!</p>
+                    <button class="medium VisualAutomotive mt35">Browse</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="grid mt35">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="newscard-header VisualGroup">
+                    <h3>Latest news</h3>
+                </div>
+                <div class="newscard">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-3">
+
+                            </div>
+                            <div class="col-lg-9">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
