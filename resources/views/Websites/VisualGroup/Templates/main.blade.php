e2275255bb79b42f5fbdea44ecd6dd7bc3d60e99
Nathan Langer <nlanger@centurioneurope.co.uk>
2018-09-05 10:44:23 +0100

    VisualGroup Page templating

---
+++ resources/views/Websites/VisualGroup/Templates/main.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+    <html lang="en">
+        <head>
+            @include('Global.Scripts.css')
+        </head>
+    <body>
+      @yield('pagecontent')
+    @include('Global.Scripts.js')
+    </body>
+</html>
