334e83b37e91eb8e4000a2651134a9e3eb3374ca
Nathan Langer <nlanger@centurioneurope.co.uk>
2018-09-05 14:38:22 +0100

    homepage & button starters

--- resources/views/Websites/VisualGroup/Pages/welcome.blade.php
+++ resources/views/Websites/VisualGroup/Pages/welcome.blade.php
@@ -1,5 +1,73 @@
 @extends('Websites.VisualGroup.Templates.main')
 @section('pagecontent')
-
+<div class="grid mt35">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-7">
+            <div class="itemcard">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <h1 class="VisualSafety">Signs &amp; Labels</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus eum commodi cupiditate in modi corrupti sunt. Autem ipsa dolorem, ex magni provident quos tenetur sit ipsam, odio impedit optio. Consequuntur!</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="/images/category/safety/signs-and-labels.jpg" alt="">
+                        </div>
+                    </div>                
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-5">
+            <div class="itemcard">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <h1 class="VisualSafety">Lockout</h1>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium magnam reprehenderit sint provident natus</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="/images/category/safety/lockout-tagout.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>    
+        </div>    
+    </div> 
+    <div class="row mt35">        
+        <div class="col-lg-5">
+            <div class="itemcard">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <h1 class="VisualSafety">Tagging</h1>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium magnam reprehenderit sint provident natus</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="/images/category/safety/tagging-systems.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>    
+        </div> 
+        <div class="col-lg-7">
+            <div class="itemcard">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <h1 class="VisualSafety">Shadow Boards</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus eum commodi cupiditate in modi corrupti sunt. Autem ipsa dolorem, ex magni provident quos tenetur sit ipsam, odio impedit optio. Consequuntur!</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="/images/category/safety/shadow-boards.jpg" alt="">
+                        </div>
+                    </div>                
+                </div>
+            </div>
+        </div>   
+    </div>
+    <hr class="mt35">     
+</div>
+</div>
 
 @endsection
