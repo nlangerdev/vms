91f371ffe551e905c3b069fc22b7cae42704542c
Nathan Langer <nlanger@centurioneurope.co.uk>
2018-09-06 12:34:19 +0100

    standard & branded footer

--- resources/views/Websites/VisualGroup/Partials/Core/footer.blade.php
+++ resources/views/Websites/VisualGroup/Partials/Core/footer.blade.php
@@ -7,3 +7,31 @@
         </div>
     </div>
 </div>
+<footer class="VisualGroup">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="footer-brand">
+                    <h4>Visual Group</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem velit similique fugit, possimus nisi ducimus provident.</p>
+                    <img src="/images/branding/footer/VisualGroup.png" alt="">
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <h4>Visual Group</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem velit similique fugit, possimus nisi ducimus provident.</p>
+            </div>            
+            <div class="col-lg-3">
+                <h4>Visual Group</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem velit similique fugit, possimus nisi ducimus provident.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="copyright">
+                    <p class="copyright text-center">Copyright &copy; {{date ('Y')}} Visual Group Ltd. All Rights Reserved.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
